80ee8c4b35bfcb49d8960895201689d7434fa7cc
Sven Sporer <sohooo@intothespirit.com>
2010-12-05 10:34:40 +0100

    merge with blueprint prototype

---
+++ compass/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+project_path = "."
+sass_dir = "content/assets/css"
+css_dir = "output/assets/css"
+images_dir = "output/assets/images"
+javascripts_dir = "content/assets/js"
+
+http_images_dir = "assets/images"
+
+sass_options = {
+  :syntax => :scss
+}
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
